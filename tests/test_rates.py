d65af4aba157bdf39ec39c4b7c61d04de3c819b3
Paweł Cichowski <cichowski.pawel1@gmail.com>
2023-04-25 20:05:51 +0200

    tests: test_rates.py: add more test cases

--- tests/test_rates.py
+++ tests/test_rates.py
@@ -5,7 +5,7 @@ from api.api import app
 test_client = TestClient(app)
 
 
-def test_average_rate_for_date_1():
+def test_average_rate_for_date_dkk():
     response = test_client.get('/rates/dkk/average/2023-04-04')
 
     assert response.status_code == 200
@@ -14,10 +14,24 @@ def test_average_rate_for_date_1():
     }
 
 
-def test_average_rate_for_date_2():
+def test_average_rate_for_date_usd():
     response = test_client.get('/rates/usd/average/2016-04-04')
 
     assert response.status_code == 200
     assert response.json() == {
         'average_rate': '3.7254'
     }
+
+
+def test_average_rate_for_date_bad_currency_code():
+    response = test_client.get('/rates/abcd/average/2016-04-04')
+
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Currency code: abcd in wrong format'}
+
+
+def test_average_rate_for_date_bad_date_format():
+    response = test_client.get('/rates/usd/average/04-04-2021')
+
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Date: 04-04-2021 in wrong format, use YYYY-MM-DD'}
